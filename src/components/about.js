648b517d72b57528df15537be5cf9c0e3255ec4a
Thian Lopez Z <thianlopezz@gmail.com>
2020-08-09 10:09:38 -0600

    aadssad

--- src/components/about.js
+++ src/components/about.js
@@ -2,12 +2,12 @@ import React from "react"
 import styles from "./about.module.css"
 
 export default () => (
-  <>
+  <div style={{ textAlign: "center" }}>
     <img src="/logo.png" width="200px" />
     {/* <h1 className={styles.headline}>
       Ecodelivery <br /> Galápagos
     </h1> */}
-    <p className={styles.underline}>Lo hacemos por ti.</p>
-    <p className={styles.underline}>Muy pronto.</p>
-  </>
+    <p className={styles.underline}>Tu pide, nosotros lo hacemos por ti.</p>
+    <p className={styles.underline}>¡Ya estamos aquí!.</p>
+  </div>
 )
--- src/pages/app.js
+++ src/pages/app.js
@@ -1,24 +1,49 @@
 import React from "react"
-import styles from "./app.module.css"
+import Particles from "react-particles-js"
+// import styles from "./app.module.css"
+import particleOptions from "../../assets/particles.json"
+import styles from "./index.module.css"
 
 export default () => (
-  <div className={styles.container}>
-    <div className={styles.contenido}>
-      <h2>¡Ya estamos aquí!</h2>
-      <h1>Descarga la App</h1>
-      <a
-        href="https://play.google.com/store/apps/details?id=com.ecodelivery.market.app"
-        target="_blank"
-      >
-        <img src="/logo.png" width="200px" />
-      </a>
+  <>
+    <Particles className={styles.particles} params={particleOptions} />
+    <div className={styles.outerContainer}>
+      <div className={styles.container}>
+        <div style={{ textAlign: "center" }} className={styles.contenido}>
+          <h2>
+            <a
+              style={{ color: "#FFFFFF" }}
+              href="https://play.google.com/store/apps/details?id=com.ecodelivery.market.app"
+              target="_blank"
+            >
+              ¡Ya estamos aquí!
+            </a>
+          </h2>
+          <a
+            href="https://play.google.com/store/apps/details?id=com.ecodelivery.market.app"
+            target="_blank"
+          >
+            <img src="/logo.png" width="200px" />
+          </a>
 
-      <a
-        href="https://play.google.com/store/apps/details?id=com.ecodelivery.market.app"
-        target="_blank"
-      >
-        <img src="/google-play.png" width="200px" />
-      </a>
+          <h1>
+            <a
+              style={{ color: "#FFFFFF" }}
+              href="https://play.google.com/store/apps/details?id=com.ecodelivery.market.app"
+              target="_blank"
+            >
+              Descarga la App
+            </a>
+          </h1>
+
+          <a
+            href="https://play.google.com/store/apps/details?id=com.ecodelivery.market.app"
+            target="_blank"
+          >
+            <img src="/google-play.png" width="200px" />
+          </a>
+        </div>
+      </div>
     </div>
-  </div>
+  </>
 )
